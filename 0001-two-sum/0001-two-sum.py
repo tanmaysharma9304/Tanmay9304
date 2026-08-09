4a380a787c024b34d4906e54683903a74ce074a8
Tanmay Sharma <tanmaysharma9304@gmail.com>
2026-08-09 15:47:26 +0530

    Time: 2118 ms (34.64%), Space: 13.2 MB (53%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,8 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        seen = {}
 
-        for i, num in enumerate(nums):
-            complement = target - num
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
 
-            if complement in seen:
-                return [seen[complement], i]
-
-            seen[num] = i
+                if nums[i] + nums[j] == target:
+                    return [i, j]
