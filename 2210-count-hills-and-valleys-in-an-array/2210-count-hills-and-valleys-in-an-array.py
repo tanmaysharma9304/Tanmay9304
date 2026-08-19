aa9616e0eccd97b59ec77026d60713e1465d8ac3
Tanmay Sharma <tanmaysharma9304@gmail.com>
2026-08-19 23:01:39 +0530

    Time: 0 ms (100%), Space: 12.2 MB (96.93%) - LeetHub

---
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def countHillValley(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        new = []
+
+        # Same consecutive numbers ko ek hi baar rakhenge
+        for num in nums:
+            if len(new) == 0 or new[-1] != num:
+                new.append(num)
+
+        count = 0
+        for i in range(1, len(new) - 1):
+
+            if new[i] > new[i - 1] and new[i] > new[i + 1]:
+                count += 1
+
+            elif new[i] < new[i - 1] and new[i] < new[i + 1]:
+                count += 1
+
+        return count
