265482208ad695a0e2c8b9a288cbe0beb9b57156
Tanmay Sharma <tanmaysharma9304@gmail.com>
2026-08-23 16:30:43 +0530

    Time: 143 ms (5.81%), Space: 12.5 MB (75.58%) - LeetHub

--- 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.py
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.py
@@ -3,32 +3,20 @@ class Solution:
 
         arr = []
 
-        # value + original index
         for i in range(len(nums)):
             arr.append((nums[i], i))
 
-        # values ko ascending order mein sort karo
         arr.sort()
-
-        # reverse karke largest values pehle
         arr = arr[::-1]
 
-        # top k elements
-        selected = arr[:k]
+        top = arr[:k]
 
-        # selected ko original index ke according sort karo
-        for i in range(len(selected)):
-            for j in range(i + 1, len(selected)):
-
-                if selected[i][1] > selected[j][1]:
-                    temp = selected[i]
-                    selected[i] = selected[j]
-                    selected[j] = temp
-
-        # sirf values answer mein
         ans = []
 
-        for value, index in selected:
-            ans.append(value)
+        for i in range(len(nums)):
+            for value, index in top:
+                if i == index:
+                    ans.append(value)
 
-        return ans
+                    if len(ans) == k:
+                        return ans
